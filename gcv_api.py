6881bfa48320bdd9b17154a0fffd4e12a29318aa
swowko51 <swowko51@gmail.com>
2019-04-15 11:05:53 -0400

    Google Cloud Vision API
    Script to run the cloud vision api against images from the Met collection to get labels for comparison.

---
+++ gcv_api.py
@@ -0,0 +1,47 @@
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
+
+
+
+
+# import io
+# import os
+
+# # Imports the Google Cloud client library
+# from google.cloud import vision
+# from google.cloud.vision import types
+
+# credential_path = "Testing API-60e5775d1e75.json"
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = crendential_path
+
+# # Instantiates a client
+# client = vision.ImageAnnotatorClient()
+
+# # The name of the image file to annotate
+# file_name = os.path.join(
+#     os.path.dirname(__file__),
+#     'Desktop/DP102947.jpg')
+
+# # Loads the image into memory
+# with io.open(file_name, 'rb') as image_file:
+#     content = image_file.read()
+
+# image = types.Image(content=content)
+
+# # Performs label detection on the image file
+# response = client.label_detection(image=image)
+# labels = response.label_annotations
+
+# print('Labels:')
+# for label in labels:
+#     print(label.description)
